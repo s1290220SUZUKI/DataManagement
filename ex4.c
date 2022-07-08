01a1cc250aa1455e2274eecbfb98f08fb86a115d
s1290220SUZUKI <108910951+s1290220SUZUKI@users.noreply.github.com>
2022-07-08 11:57:18 +0900

    HoT Initial revision

---
+++ ex4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int i,num,heads=0,tails=0;
+
+printf("Tossing a coin...\n");
+
+ for(i=0;i<3;i++){
+num = rand();
+if(num%2==0){
+printf("Round %d:Heds\n",i+1);
+heads++;
+}else{
+printf("Round %d: Tails\n",i+1);
+tails++;
+}
+}
+printf("Heads: %d, Tails:%d\n",heads,tails);
+return 0;
+}
